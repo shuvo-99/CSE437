b98af8842b074d83e727293d335ddd95b388c4aa
shuvo-99 <anontoshuvo99@gmail.com>
2023-07-09 11:33:39 +0600

    update

--- Unsupervised Learning in Python.py
+++ Unsupervised Learning in Python.py
@@ -547,7 +547,25 @@ for component in model.components_:
     show_as_image(component)
     
 # ------------------------------------------------
-# 
+# Perform the necessary imports
+import pandas as pd
+from sklearn.preprocessing import normalize
+ 
+# Normalize the NMF features: norm_features
+norm_features = normalize(nmf_features)
+ 
+# Create a DataFrame: df
+df = pd.DataFrame(norm_features, index=titles)
+ 
+# Select the row corresponding to 'Cristiano Ronaldo': article
+article = df.loc['Cristiano Ronaldo',:]
+ 
+# Compute the dot products: similarities
+similarities = df.dot(article)
+ 
+# Display those with the largest cosine similarity
+print(similarities.nlargest())
+
 # ------------------------------------------------
 # 
 # ------------------------------------------------
