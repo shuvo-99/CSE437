ebb2c6d2c61b8ef4797f6b5eb4142499f9ccf5ef
shuvo-99 <anontoshuvo99@gmail.com>
2023-07-09 11:34:58 +0600

    update

--- Unsupervised Learning in Python.py
+++ Unsupervised Learning in Python.py
@@ -588,7 +588,21 @@ pipeline = make_pipeline(scaler, nmf, normalizer)
 norm_features = pipeline.fit_transform(artists)
 
 # ------------------------------------------------
-# 
+# Import pandas
+import pandas as pd
+ 
+# Create a DataFrame: df
+df = pd.DataFrame(norm_features, index=artist_names)
+ 
+# Select row of 'Bruce Springsteen': artist
+artist = df.loc['Bruce Springsteen']
+ 
+# Compute cosine similarities: similarities
+similarities = df.dot(artist)
+ 
+# Display those with highest cosine similarity
+print(similarities.nlargest())
+
 # ------------------------------------------------
 # 
 # ------------------------------------------------
