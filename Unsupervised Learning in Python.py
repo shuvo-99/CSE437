e5137ed5e66b8f049d0dae38cbd3779a134bfcea
shuvo-99 <anontoshuvo99@gmail.com>
2023-07-09 11:20:54 +0600

    update

--- Unsupervised Learning in Python.py
+++ Unsupervised Learning in Python.py
@@ -372,7 +372,21 @@ plt.show()
 2
 
 # ------------------------------------------------
-# 
+# Import PCA
+from sklearn.decomposition import PCA
+ 
+# Create a PCA model with 2 components: pca
+pca = PCA(n_components=2)
+ 
+# Fit the PCA instance to the scaled samples
+pca.fit(scaled_samples)
+ 
+# Transform the scaled samples: pca_features
+pca_features = pca.transform(scaled_samples)
+ 
+# Print the shape of pca_features
+print(pca_features.shape)
+
 # ------------------------------------------------
 # 
 # ------------------------------------------------
