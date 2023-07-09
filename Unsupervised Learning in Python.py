7eceb45ae937cd61a8243ebb583aa6129c2ef835
shuvo-99 <anontoshuvo99@gmail.com>
2023-07-09 10:50:39 +0600

    update

--- Unsupervised Learning in Python.py
+++ Unsupervised Learning in Python.py
@@ -61,7 +61,21 @@ plt.xticks(ks)
 plt.show()
 
 # ----------------------------
-# 
+# Create a KMeans model with 3 clusters: model
+model = KMeans(n_clusters=3)
+ 
+# Use fit_predict to fit model and obtain cluster labels: labels
+labels = model.fit_predict(samples)
+ 
+# Create a DataFrame with labels and varieties as columns: df
+df = pd.DataFrame({'labels': labels, 'varieties': varieties})
+ 
+# Create crosstab: ct
+ct = pd.crosstab(df['labels'], df['varieties'])
+ 
+# Display ct
+print(ct)
+
 # ----------------------------
 # 
 # ----------------------------
