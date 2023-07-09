13b39db5506a58c7aeb5e000f63d900e4de652a0
shuvo-99 <anontoshuvo99@gmail.com>
2023-07-09 11:26:20 +0600

    update

--- Unsupervised Learning in Python.py
+++ Unsupervised Learning in Python.py
@@ -442,7 +442,22 @@ print(df.sort_values('label'))
 1 and 3
 
 # ------------------------------------------------
-# 
+# Import NMF
+from sklearn.decomposition import NMF
+ 
+# Create an NMF instance: model
+model = NMF(n_components=6)
+ 
+# Fit the model to articles
+model.fit(articles)
+ 
+# Transform the articles: nmf_features
+nmf_features = model.transform(articles)
+
+# Print the NMF features
+print(nmf_features.round(2))
+
+
 # ------------------------------------------------
 # 
 # ------------------------------------------------
