d8d8f53b00267e94d862c12ba9c12f677e459a11
shuvo-99 <anontoshuvo99@gmail.com>
2023-07-09 10:51:39 +0600

    update

--- Unsupervised Learning in Python.py
+++ Unsupervised Learning in Python.py
@@ -77,7 +77,20 @@ ct = pd.crosstab(df['labels'], df['varieties'])
 print(ct)
 
 # ----------------------------
-# 
+# Perform the necessary imports
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+ 
+# Create scaler: scaler
+scaler = StandardScaler()
+ 
+# Create KMeans instance: kmeans
+kmeans = KMeans(n_clusters=4)
+ 
+# Create pipeline: pipeline
+pipeline = make_pipeline(scaler, kmeans)
+
 # ----------------------------
 # 
 # ----------------------------
