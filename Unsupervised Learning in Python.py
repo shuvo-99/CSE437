2d2c29a0feae895653631bdd054b8a1859a9de7c
shuvo-99 <anontoshuvo99@gmail.com>
2023-07-09 11:32:56 +0600

    update

--- Unsupervised Learning in Python.py
+++ Unsupervised Learning in Python.py
@@ -533,7 +533,19 @@ digit_features = features[0,:]
 print(digit_features)
 
 # ------------------------------------------------
-# 
+# Import PCA
+from sklearn.decomposition import PCA
+ 
+# Create a PCA instance: model
+model = PCA(n_components=7)
+ 
+# Apply fit_transform to samples: features
+features = model.fit_transform(samples)
+ 
+# Call show_as_image on each component
+for component in model.components_:
+    show_as_image(component)
+    
 # ------------------------------------------------
 # 
 # ------------------------------------------------
