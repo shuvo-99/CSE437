1760f9385d392e118931dc8ab06514f5584f9974
shuvo-99 <anontoshuvo99@gmail.com>
2023-07-09 10:47:18 +0600

    update

--- Unsupervised Learning in Python.py
+++ Unsupervised Learning in Python.py
@@ -2,6 +2,7 @@
 3
 
 # ----------------------------
+
 # Import KMeans
 from sklearn.cluster import KMeans
  
@@ -18,8 +19,29 @@ labels = model.predict(new_points)
 print(labels)
 
 # ----------------------------
-# 
+# Import pyplot
+import matplotlib.pyplot as plt
+ 
+# Assign the columns of new_points: xs and ys
+xs = new_points[:,0]
+ys = new_points[:,1]
+ 
+# Make a scatter plot of xs and ys, using labels to define the colors
+plt.scatter(xs, ys, c=labels, alpha=0.5)
+ 
+# Assign the cluster centers: centroids
+centroids = model.cluster_centers_
+ 
+# Assign the columns of centroids: centroids_x, centroids_y
+centroids_x = centroids[:,0]
+centroids_y = centroids[:,1]
+ 
+# Make a scatter plot of centroids_x and centroids_y
+plt.scatter(centroids_x, centroids_y, marker='D', s=50)
+plt.show()
+
 # ----------------------------
+
 # 
 # ----------------------------
 # 
